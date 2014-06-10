001835ccf0e9c98806f9fd36336a63aa6d093859
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-06-10 14:25:10 -0700

    Stub a spec for Promise class

---
+++ RxSwiftTests/PromiseSpec.swift
@@ -0,0 +1,17 @@
+//
+//  PromiseSpec.swift
+//  RxSwift
+//
+//  Created by Justin Spahr-Summers on 2014-06-10.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+import Quick
+
+class PromiseSpec: QuickSpec {
+	override class func exampleGroups() {
+		it("should pass") {
+			expect(true).to.beTrue()
+		}
+	}
+}
