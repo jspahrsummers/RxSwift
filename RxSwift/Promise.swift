4f913b4612caed57c7fe84534b7b72556edc7c33
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-06-10 23:30:32 -0700

    Implement Promise.whenFinished

--- RxSwift/Promise.swift
+++ RxSwift/Promise.swift
@@ -12,19 +12,36 @@ enum PromiseState<T> {
 	case Unresolved(() -> T)
 	case Resolving
 	case Done(() -> T)
-}
-
-class Promise<T> {
-	let condition = NSCondition()
 	
-	var state: PromiseState<T>
 	var isResolving: Bool {
 		get {
-			switch self.state {
+			switch self {
 			case .Resolving:
 				return true
 			default:
 				return false
+			}
+		}
+	}
+}
+
+class Promise<T> {
+	let condition = NSCondition()
+	var finishedActions: (() -> ())[] = []
+	
+	var state: PromiseState<T> {
+		willSet(newState) {
+			switch newState {
+			case .Done:
+				for action in self.finishedActions {
+					action()
+				}
+				
+				self.finishedActions = []
+				self.condition.broadcast()
+				
+			default:
+				break
 			}
 		}
 	}
@@ -41,6 +58,7 @@ class Promise<T> {
 				case let .Unresolved(work):
 					self.state = .Resolving
 					return work
+					
 				case let .Done(result):
 					fallthrough
 				case .Resolving:
@@ -56,14 +74,13 @@ class Promise<T> {
 			
 			withLock(self.condition) { () -> () in
 				self.state = PromiseState.Done { result }
-				self.condition.broadcast()
 			}
 		}
 	}
 	
 	func result() -> T {
 		let (maybeWork: (() -> T)?, maybeResult: T?) = withLock(self.condition) {
-			while self.isResolving {
+			while self.state.isResolving {
 				self.condition.wait()
 			}
 
@@ -71,8 +88,10 @@ class Promise<T> {
 			case let .Unresolved(work):
 				self.state = .Resolving
 				return (work, nil)
+				
 			case let .Done(result):
 				return (nil, result())
+				
 			default:
 				return (nil, nil)
 			}
@@ -83,7 +102,6 @@ class Promise<T> {
 
 			withLock(self.condition) { () -> () in
 				self.state = PromiseState.Done { () -> T in result }
-				self.condition.broadcast()
 			}
 
 			return result
@@ -93,6 +111,27 @@ class Promise<T> {
 	}
 	
 	func whenFinished(scheduler: Scheduler, action: T -> ()) -> Disposable? {
-		return nil
+		return withLock(self.condition) {
+			switch self.state {
+			case let .Done(result):
+				return scheduler.schedule {
+					action(result())
+				}
+			
+			default:
+				let disposable = CompositeDisposable()
+			
+				// TODO: Offer a way to remove this from the array too?
+				self.finishedActions.append {
+					let schedulerDisposable = scheduler.schedule {
+						action(self.result())
+					}
+					
+					disposable.addDisposable(schedulerDisposable)
+				}
+				
+				return disposable
+			}
+		}
 	}
 }
