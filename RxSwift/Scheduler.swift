55e80e6403adc1baaf64362ce064b4c88e2a076c
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-06-15 23:24:59 -0700

    Simplify Scheduler definitions

--- RxSwift/Scheduler.swift
+++ RxSwift/Scheduler.swift
@@ -9,40 +9,44 @@
 import Foundation
 
 /// Represents a serial queue of work items.
-@class_protocol
 protocol Scheduler {
-	/// Enqueues the given work item.
+	/// Enqueues an unannotated action on the scheduler.
 	///
-	/// When the work is executed depends on the specific implementation.
+	/// When the work is executed depends on the scheduler in use.
 	///
 	/// Optionally returns a disposable that can be used to cancel the work
 	/// before it begins.
-	func schedule(work: () -> ()) -> Disposable?
+	func schedule(action: () -> ()) -> Disposable?
 }
 
 /// A scheduler that performs all work synchronously.
-@final class ImmediateScheduler: Scheduler {
-	func schedule(work: () -> ()) -> Disposable? {
-		work()
+struct ImmediateScheduler: Scheduler {
+	func schedule(action: () -> ()) -> Disposable? {
+		action()
 		return nil
 	}
 }
 
+/// A scheduler that performs all work on the main thread.
+struct MainScheduler: Scheduler {
+	func schedule(action: () -> ()) -> Disposable? {
+		let d = SimpleDisposable()
+
+		dispatch_async(dispatch_get_main_queue(), {
+			if d.disposed {
+				return
+			}
+
+			action()
+		})
+
+		return d
+	}
+}
+
 /// A scheduler backed by a serial GCD queue.
-@final class QueueScheduler: Scheduler {
+struct QueueScheduler: Scheduler {
 	let _queue = dispatch_queue_create("com.github.RxSwift.QueueScheduler", DISPATCH_QUEUE_SERIAL)
-
-	struct _Shared {
-		static let mainThreadScheduler = QueueScheduler(dispatch_get_main_queue())
-	}
-
-	/// A specific kind of QueueScheduler that will run its work items on the
-	/// main thread.
-	class var mainThreadScheduler: QueueScheduler {
-		get {
-			return _Shared.mainThreadScheduler
-		}
-	}
 
 	/// Initializes a scheduler that will target the given queue with its work.
 	///
@@ -54,13 +58,13 @@ protocol Scheduler {
 	
 	/// Initializes a scheduler that will target the global queue with the given
 	/// priority.
-	convenience init(_ priority: CLong) {
+	init(_ priority: CLong) {
 		self.init(dispatch_get_global_queue(priority, 0))
 	}
 	
 	/// Initializes a scheduler that will target the default priority global
 	/// queue.
-	convenience init() {
+	init() {
 		self.init(DISPATCH_QUEUE_PRIORITY_DEFAULT)
 	}
 	
