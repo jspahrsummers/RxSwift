d7c723bb2e5a083480ee7d071aa8d1b656d8605a
Masayuki Ono <mono0926@gmail.com>
2014-06-20 04:13:21 +0900

    update the Quick submodule and modify test codes to build on Xcode 6 beta 2.

--- RxSwiftTests/PromiseSpec.swift
+++ RxSwiftTests/PromiseSpec.swift
@@ -7,20 +7,20 @@
 //
 
 import Quick
+import Nimble
 import RxSwift
 
 class PromiseSpec: QuickSpec {
-	override class func exampleGroups() {
-		var promise: Promise<Int>!
-		
-		beforeEach {
-			promise = Promise {
-				return 5
-			}
-		}
-	
-		it("should block and return a result") {
+    override func spec() {
+        var promise: Promise<Int>!
+        
+        beforeEach {
+            promise = Promise {
+                return 5
+            }
+        }
+        it("should block and return a result") {
             expect(promise.result()).to.equal(5)
-		}
+        }
 	}
 }
